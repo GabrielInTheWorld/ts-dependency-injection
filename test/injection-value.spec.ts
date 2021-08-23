680bebdede18547ec884efcdc8e858d492d2266a
GabrielMeyer <meyergabriel@live.de>
2021-08-23 17:46:07 +0200

    Update: Tests enhanced

--- test/injection-value.spec.ts
+++ test/injection-value.spec.ts
@@ -14,21 +14,22 @@ class TestClass {
     }
   })
   public readonly helloworld: string;
+  public modifiedValue: string;
 
   @Inject(TestContructedClass)
   public readonly testInterface: TestInterface;
 
-  public constructor() {
-    console.log('Called TestClass:constructor', this.helloworld);
-  }
+  public constructor() {}
 
-  public testFn(provider: string): void {
+  public testFn = (provider: string): void => {
     console.log(`Hello ${provider}`);
-  }
+    this.modifiedValue = 'modified';
+  };
 }
 
 test('InjectionValue', () => {
   const test2 = DiContainer.get(TestClass);
   expect(test2.helloworld).toBe('world');
   expect(test2.testInterface).toBeTruthy();
+  expect(test2.modifiedValue).toBe('modified');
 });
